205a49a6c8c9248d45c936d407252c85e373b6bb
SouzaGuilherme <gdsdsilva@inf.ufpel.edu.br>
2021-12-14 13:05:07 -0300

    simple GKE configuration using min 2 max 3 nodes from n2-standard-2.

---
+++ main.tf
@@ -0,0 +1,78 @@
+module "kubernetes-engine" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  version = "17.3.0"
+  project_id                 = var.gcp_project_id
+  name                       = var.gcp_cluster_name
+  region                     = var.gcp_region
+  regional                   = var.gke_regional
+  zones                      = var.gke_zone
+  network                    = var.gke_network
+  subnetwork                 = var.gke_subnetwork
+  ip_range_pods              = ""
+  ip_range_services          = ""
+  http_load_balancing        = false
+  horizontal_pod_autoscaling = true
+  network_policy             = false
+
+  node_pools = [
+    {
+      name                      = var.gke_node_name_pool
+      machine_type              = var.gke_machine_type
+      min_count                 = var.initial_node_count
+      max_count                 = var.max_node_count
+      local_ssd_count           = 0
+      disk_size_gb              = var.disk_size_gb
+      disk_type                 = "pd-standard"
+      image_type                = "COS"
+      auto_repair               = true
+      auto_upgrade              = true
+      service_account           = var.gke_service_account
+      preemptible               = true
+      initial_node_count        = var.initial_node_count
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = []
+
+    default-node-pool = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
+}
