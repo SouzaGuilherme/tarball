22138a3648f6c28a45b73125199f1f9e5dc2d23f
SouzaGuilherme <gdsdsilva@inf.ufpel.edu.br>
2021-12-14 12:56:58 -0300

    GCP execution provider (GKE). using personal account for testing.

---
+++ providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~>3.89.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.gcp_credentials)
+  project = var.gcp_project_id
+  region = var.gcp_region
+  zone = var.gcp_zone
+}
+provider "google-beta" {
+  credentials = file(var.gcp_credentials)
+  project = var.gcp_project_id
+  region = var.gcp_region
+  zone = var.gcp_zone
+}
+
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.kubernetes-engine.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.kubernetes-engine.ca_certificate)
+}
